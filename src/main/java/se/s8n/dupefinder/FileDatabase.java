bc63128ae89ca77893c1a6329fe6fbf48b74ba2b
Olof Samuelsson <olof.samuelsson@gmail.com>
2014-06-04 21:53:40 +0200

    Added some file walking

--- src/main/java/se/s8n/dupefinder/FileDatabase.java
+++ src/main/java/se/s8n/dupefinder/FileDatabase.java
@@ -1,13 +1,41 @@
 package se.s8n.dupefinder;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FileDatabase {
-	String dbLocation;
+	@SuppressWarnings("unused")
+	private String dbLocation;
+	private Map<String, String> db = new HashMap<String, String>();
 
 	FileDatabase(String dbLocation) {
 		this.dbLocation = dbLocation;
 	}
 
 	boolean createDB(String dbRoot) {
+		SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file,
+					BasicFileAttributes attrs) throws IOException {
+				db.put(file.toString(), String.valueOf(attrs.size()));
+				return FileVisitResult.CONTINUE;
+			}
+
+		};
+		try {
+			@SuppressWarnings("unused")
+			Path xxx = Files.walkFileTree(Paths.get("."), visitor);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 
