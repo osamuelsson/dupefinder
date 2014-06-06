a52175276e99d07058e29c69c6b08a4eb3f08351
Olof Samuelsson <olof@s8n.se>
2014-06-06 13:57:01 +0200

    Add a method to find files with the same size.

--- src/main/java/se/s8n/dupefinder/FileDatabase.java
+++ src/main/java/se/s8n/dupefinder/FileDatabase.java
@@ -11,40 +11,66 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FileDatabase {
-	@SuppressWarnings("unused")
-	private String dbLocation;
-	private Map<String, String> db = new HashMap<String, String>();
 
-	FileDatabase(String dbLocation) {
-		this.dbLocation = dbLocation;
-	}
+    @SuppressWarnings("unused")
+    private final String dbLocation;
+    private Map<String, String> db = new HashMap<String, String>();
 
-	boolean createDB(String dbRoot) {
-		SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult visitFile(Path file,
-					BasicFileAttributes attrs) throws IOException {
-				db.put(file.toString(), String.valueOf(attrs.size()));
-				return FileVisitResult.CONTINUE;
-			}
+    FileDatabase(String dbLocation) {
+        this.dbLocation = dbLocation;
+    }
 
-		};
-		try {
-			@SuppressWarnings("unused")
-			Path xxx = Files.walkFileTree(Paths.get("."), visitor);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
+    boolean createDB(String dbRoot) {
+        SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file,
+                    BasicFileAttributes attrs) throws IOException {
+                db.put(file.toString(), String.valueOf(attrs.size()));
+                return FileVisitResult.CONTINUE;
+            }
 
-	boolean listDB() {
-		return false;
-	}
+        };
+        try {
+            @SuppressWarnings("unused")
+            Path xxx = Files.walkFileTree(Paths.get("."), visitor);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	boolean showDuplicates() {
-		return false;
-	}
+    boolean listDB() {
+        return false;
+    }
 
+    boolean showDuplicates() {
+        return false;
+    }
+
+    boolean findDups() {
+        final Map<Long ,Path> foundFiles = new HashMap<>();
+        SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file,
+                    BasicFileAttributes attrs) throws IOException {
+                if (foundFiles.containsKey(attrs.size())) {
+                    System.out.println("Duplicates " + " " + file + " " + foundFiles.get(attrs.size()));
+                } else {
+                    foundFiles.put(attrs.size(), file);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+        };
+        try {
+            @SuppressWarnings("unused")
+            Path xxx = Files.walkFileTree(Paths.get("."), visitor);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return false;
+
+    }
 }
